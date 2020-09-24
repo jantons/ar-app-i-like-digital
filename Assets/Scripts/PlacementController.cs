0f177ef8a5627f47e9b751dcf40274cfc193f313
Danish Ali <syeddanishdev@gmail.com>
2020-09-25 01:52:23 +0200

    Build 0.2
    Plane Detection has been added
    Tap to spawn with ray casting has been implemented
    Image scan approach has to be implemented.

---
+++ Assets/Scripts/PlacementController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject placedPrefab;
+    ARRaycastManager aRRaycastManager;
+
+    private void Awake()
+    {
+        aRRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+
+    public GameObject PlacedPrefab
+    {
+        get { return placedPrefab; }
+        set { placedPrefab = value; }
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;
+    }
+    void Update()
+    {
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+            return;
+
+
+        if (aRRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+        {
+            var hitPos = hits[0].pose;
+            Instantiate(placedPrefab, hitPos.position, hitPos.rotation);
+        }
+
+
+
+    }
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+}
+
+
